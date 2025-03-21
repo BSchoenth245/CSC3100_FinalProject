c8f3fed77ddd84b9831d2a7f257e4ccc5d49d509
BSchoenth245 <brockschoenthaler@gmail.com>
2025-03-21 00:28:19 -0500

    Updated index, added app.js with login logic

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$("#btnLogin").on('click',function(){
+    // Regular expression for emails
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+
+    // Captures necessary Login data
+    let strUsername = document.querySelector('#txtLogUser').value
+    const strPassword = $('#txtLogPass').val()
+    let blnError = false
+    let strMessage = ""
+
+    strUsername = strUsername.toLowerCase()
+    // Input validation for login
+    if(!regEmail.test(strUsername)){
+        blnError = true
+        strMessage+='<p class ="mb-0 mt-0">Username must be an email address</p>'
+    }
+    if(strPassword.length < 8){
+        blnError = true
+        strMessage+="<p>Password cannot be blank</p>"
+    }
+    // Error message
+    if(blnError == true){
+        Swal.fire({
+            title: "Oh no, you have an error!",
+            html: strMessage,
+            icon: "error"
+        })
+    }
+    // Success message
+    else{
+        Swal.fire({
+            title: "Congrats!",
+            html:  "You're logged in!",
+            icon: "success"
+        })
+    }
+})
+
+// Reveals/hides password on the login page
+function ViewLogPass() {
+    var x = document.getElementById("txtLogPassword");
+    if (x.type === "password") {
+        $('#btnViewLogPass').removeClass('bi-eye')
+        document.querySelector('#btnViewLogPass').classList.add('bi-eye-slash')
+        x.type = "text";
+    } else {
+        $('#btnViewLogPass').removeClass('bi-eye-slash')
+        document.querySelector('#btnViewLogPass').classList.add('bi-eye')
+        x.type = "password";
+    }
+}
