fdd361645e07c74face1a524036468ba4e3d0af3
Fallenstarfish0 <korodymitchell@gmail.com>
2025-03-24 22:44:36 -0500

    updated btn logic

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
 $("#btnLogin").on('click',function(){
     // Regular expression for emails
     const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    const regPass = ~/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+    //const regPass = ~/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
 
     // Captures necessary Login data
     let strUsername = document.querySelector('#txtLogUser').value
@@ -23,7 +23,7 @@ $("#btnLogin").on('click',function(){
         blnError = true
         strMessage+='<p class ="mb-0 mt-0">Username must be an email address</p>'
     }
-    if(!regPass.test(strPassword)){
+    if(strPassword.length < 8){
         blnError = true
         strMessage+="<p>Password cannot be blank</p>"
     }
@@ -43,6 +43,62 @@ $("#btnLogin").on('click',function(){
             icon: "success"
         })
     }
+})
+
+$("#btnRegister").on('click',function(){
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    //const regPass = ~/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+
+    // Captures necessary Login data
+    let strUsername = document.querySelector('#txtRegUsername').value
+    let strFirst = document.querySelector('#txtFirstName').value
+    let strLast = document.querySelector('#txtLastName').value
+
+    const strPassword = $('#txtRegPassword').val()
+    const strConPassword = $('#txtConfirmPassword').val()
+
+    let blnError = false
+    let strMessage = ""
+
+    strUsername = strUsername.toLowerCase()
+    // Input validation for login
+    if(strFirst.trim().length < 1){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">First Name Cannot Be Blank</p>'            
+    }
+    if(strLast.trim().length < 1){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">First Name Cannot Be Blank</p>'            
+    }
+    if(!regEmail.test(strUsername)){
+        blnError = true
+        strMessage+='<p class ="mb-0 mt-0">Username must be an email address</p>'
+    }
+    if(strPassword.length < 8){
+        blnError = true
+        strMessage+="<p>Password cannot be blank</p>"
+    }
+    if(strConPassword != strPassword){
+        blnError = true
+        strMessage+="<p>Passwords must match</p>"
+    }
+    // Error message
+    if(blnError == true){
+        Swal.fire({
+            title: "Oh no, you have an error!",
+            html: strMessage,
+            icon: "error"
+        })
+    }
+    // Success message
+    else{
+        Swal.fire({
+            title: "Congrats!",
+            html:  "You're logged in!",
+            icon: "success"
+        })
+    }
+
 })
 
 // Reveals/hides password on the login page
